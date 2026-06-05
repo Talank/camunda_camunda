401f7cd97e057198f87938a471aa09da06ef9edb
Christoph Fricke <christoph.fricke@holisticon.de>
2026-06-05 13:32:56 +0200

    feat: use camunda-api-zod-schemas for parsing document references

--- operate/client/src/App/ProcessInstance/BottomPanelTabs/VariablesTab/Variables/DocumentPreview/DocumentPreviewModal.tsx
+++ operate/client/src/App/ProcessInstance/BottomPanelTabs/VariablesTab/Variables/DocumentPreview/DocumentPreviewModal.tsx
@@ -35,6 +35,10 @@ const DocumentPreviewModal: React.FC<StateProps & DocumentPreviewProps> = ({
 };
 
 const ModalContent: React.FC<DocumentPreviewProps> = ({document}) => {
+  if (document.link === null) {
+    return <p>Preview not available for document "{document.fileName}".</p>;
+  }
+
   if (document.type === 'image') {
     return <PreviewImage src={document.link} alt={document.fileName} />;
   }
@@ -44,7 +48,7 @@ const ModalContent: React.FC<DocumentPreviewProps> = ({document}) => {
   }
 
   if (document.type === 'json') {
-    return <PreviewJson document={document} />;
+    return <PreviewJson src={document.link} fileName={document.fileName} />;
   }
 
   return <p>Preview not available for document "{document.fileName}".</p>;
