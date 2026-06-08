3c3550b0246fd00693efd0a05a8cd3b3a964e3a0
Dmitri Nikonov <dmitri.nikonov@camunda.com>
2026-06-08 12:22:21 +0200

    refactor: rename resolveDocuments to attachDocuments around the judge concept

--- testing/camunda-process-test-java/src/main/java/io/camunda/process/test/api/CamundaProcessTestExtension.java
+++ testing/camunda-process-test-java/src/main/java/io/camunda/process/test/api/CamundaProcessTestExtension.java
@@ -232,8 +232,8 @@ public class CamundaProcessTestExtension
                         adapter,
                         CamundaProcessTestRuntimeDefaults.JUDGE_PROPERTIES.getThreshold(),
                         CamundaProcessTestRuntimeDefaults.JUDGE_PROPERTIES.getCustomPrompt())
-                    .withResolveDocuments(
-                        CamundaProcessTestRuntimeDefaults.JUDGE_PROPERTIES.isResolveDocuments()))
+                    .withAttachDocuments(
+                        CamundaProcessTestRuntimeDefaults.JUDGE_PROPERTIES.isAttachDocuments()))
         .ifPresent(CamundaAssert::setJudgeConfig);
   }
 
